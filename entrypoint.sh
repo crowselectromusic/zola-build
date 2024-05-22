243058b19249b73825072ce14c9f3e7f297c570b
Cory Alder <cory@davander.com>
2024-05-21 23:11:12 -0700

    Add base_url parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,12 @@ set -o pipefail
 
 main() {
     echo "Running zola with root ${INPUT_ROOT}"
-    zola --root "${INPUT_ROOT}" build
+    if [[ -n "${INPUT_BASE_URL}" ]];  then
+        echo "Running zola with base_url ${INPUT_BASE_URL}"
+        zola --root "${INPUT_ROOT}" build --base-url "${INPUT_BASE_URL}"
+    else
+        zola --root "${INPUT_ROOT}" build 
+    fi
 }
 
 main "$@"
